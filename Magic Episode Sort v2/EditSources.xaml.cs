ffcd382d3db45e11b77f83c7d0271c846520696d
Critical Session <criticalsession@protonmail.ch>
2022-10-31 08:02:04 +0100

    edit sources window

--- Magic Episode Sort v2/EditSources.xaml.cs
+++ Magic Episode Sort v2/EditSources.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +12,67 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using TheMagic;
 
 namespace Magic_Episode_Sort_v2
 {
-    /// <summary>
-    /// Interaction logic for EditSources.xaml
-    /// </summary>
     public partial class EditSources : Window
     {
         public EditSources()
         {
             InitializeComponent();
+            lstSources.ItemsSource = Settings.SourceDirectories;
+        }
+
+        private void btnAddSource_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.AddSourceDirectory(txtAddSource.Text);
+            Settings.SaveSettings();
+
+            txtAddSource.Text = "";
+            btnAddSource.IsEnabled = false;
+
+            lstSources.Items.Refresh();
+        }
+
+        private void btnAddSourceDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                txtAddSource.Text = dialog.FileName;
+                btnAddSource.IsEnabled = true;
+            }
+            else
+            {
+                txtAddSource.Text = "";
+                btnAddSource.IsEnabled = false;
+            }
+
+            this.Focus();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //FillSources();
+        }
+
+        private void lstSources_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void ctxDeleteSource_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstSources.SelectedItem != null)
+            {
+                Settings.RemoveDirectory(lstSources.SelectedItem.ToString());
+                Settings.SaveSettings();
+
+                lstSources.Items.Refresh();
+            }
         }
     }
 }
